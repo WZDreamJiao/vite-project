92b48757d1c56884a16d682d6157b90bb9ca5796
ze.wang13 <ze.wang13@gientech.com>
2023-06-06 17:27:43 +0800

    init

--- src/pages/codemirror/index.vue
+++ src/pages/codemirror/index.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>
-
-  </div>
+  <codemirror
+      v-model="code"
+      placeholder="Code gose here..."
+      :style="{ height: '400px' }"
+      :autofocus="true"
+      :indent-with-tab="true"
+      :tabSize="2"
+      :extensions="extensions"
+      @ready="log('ready', $event)"
+      @change="log('change', $event)"
+      @focus="log('focus', $event)"
+      @blur="log('blur', $event)"
+  />
 </template>
 
 <script setup>
+import {Codemirror} from "vue-codemirror";
+import {javascript} from "@codemirror/lang-javascript";
+import {oneDark} from "@codemirror/theme-one-dark";
 
+const code = ref(`console.log('Hello, world!')`);
+const extensions = [javascript()];
+const log = (type, $event) => {
+  console.log(type, $event)
+}
 </script>
 
 <style scoped>
